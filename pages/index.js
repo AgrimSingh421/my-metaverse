e089fb698d907a98a962623aaa669acfe07621f0
Agrim Singh <singhagrim36@gmail.com>
2021-12-26 19:56:06 +0530

    Chat Function Completed.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
-import Head from 'next/head'
-import Login from '../components/Login'
+import Head from "next/head";
+import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Headers from "../components/Headers";
+import Messages from "../components/Messages";
 
 export default function Home() {
   const { isAuthenticated } = useMoralis();
 
-  if(!isAuthenticated) return <Login />;
+  if (!isAuthenticated) return <Login />;
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to bg-fuchsia-900 overflow-hidden">
       <Head>
         <title>METAVERSE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <h1>Welcome to the app.</h1>
-      
+
+      <div className="max-w-screen-2xl mx-auto">
+        <Headers />
+        <Messages />
+      </div>
     </div>
-  )
+  );
 }
